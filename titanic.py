5dd44028b5ac0607339a6c8973f85855f43d8728
Wálisson Soares <wallyson2013@hotmail.com>
2023-12-06 14:54:49 -0300

    Add files via upload

--- titanic.py
+++ titanic.py
@@ -1,26 +1,30 @@
+import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Carregando os dados do Titanic
-titanic_data = pd.read_csv('train.csv')
+titanic_data = pd.read_csv('caminho/do/arquivo/train.csv')
 
 # Visualizando as primeiras linhas do DataFrame
-print(titanic_data.head())
+st.write(titanic_data.head())
 
 # Estatísticas gerais da base
-print("\nEstatísticas Gerais:")
-print(titanic_data.describe())
+st.subheader("Estatísticas Gerais:")
+st.write(titanic_data.describe())
 
 # Contagem de valores para algumas colunas
-print("\nContagem de Valores:")
-print(titanic_data['Sex'].value_counts())
-print(titanic_data['Pclass'].value_counts())
-print(titanic_data['Embarked'].value_counts())
+st.subheader("Contagem de Valores:")
+st.write(titanic_data['Sex'].value_counts())
+st.write(titanic_data['Pclass'].value_counts())
+st.write(titanic_data['Embarked'].value_counts())
 
 # Gráfico de barras para a contagem de sobreviventes
 sns.set(style="whitegrid")
 plt.figure(figsize=(8, 6))
 sns.countplot(x='Survived', data=titanic_data, palette="Set2")
 plt.title('Contagem de Sobreviventes no Titanic')
-plt.show()
+st.pyplot()
+
+# Exibindo o gráfico
+st.pyplot()
