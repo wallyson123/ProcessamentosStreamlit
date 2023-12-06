6f4100fcb4b99dbb5396f2c874cf30fb1c234767
Wálisson Soares <wallyson2013@hotmail.com>
2023-12-06 15:01:36 -0300

    Add files via upload

--- titanic.py
+++ titanic.py
@@ -1,30 +1,51 @@
-import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Carregando os dados do Titanic
-titanic_data = pd.read_csv('caminho/do/arquivo/train.csv')
+titanic_data = pd.read_csv('C:/testtitanic/train.csv')
 
 # Visualizando as primeiras linhas do DataFrame
-st.write(titanic_data.head())
+print(titanic_data.head())
 
 # Estatísticas gerais da base
-st.subheader("Estatísticas Gerais:")
-st.write(titanic_data.describe())
+print("\nEstatísticas Gerais:")
+print(titanic_data.describe())
 
 # Contagem de valores para algumas colunas
-st.subheader("Contagem de Valores:")
-st.write(titanic_data['Sex'].value_counts())
-st.write(titanic_data['Pclass'].value_counts())
-st.write(titanic_data['Embarked'].value_counts())
+print("\nContagem de Valores:")
+print(titanic_data['Sex'].value_counts())
+print(titanic_data['Pclass'].value_counts())
+print(titanic_data['Embarked'].value_counts())
 
 # Gráfico de barras para a contagem de sobreviventes
 sns.set(style="whitegrid")
-plt.figure(figsize=(8, 6))
-sns.countplot(x='Survived', data=titanic_data, palette="Set2")
-plt.title('Contagem de Sobreviventes no Titanic')
-st.pyplot()
+plt.figure(figsize=(12, 6))
 
-# Exibindo o gráfico
-st.pyplot()
+# Contagem de sobreviventes por classe
+plt.subplot(2, 3, 1)
+sns.countplot(x='Survived', hue='Pclass', data=titanic_data, palette="Set2")
+plt.title('Contagem de Sobreviventes por Classe')
+
+# Distribuição de idades
+plt.subplot(2, 3, 2)
+sns.histplot(titanic_data['Age'].dropna(), kde=True, bins=30, color='blue')
+plt.title('Distribuição de Idades')
+
+# Contagem de sobreviventes por sexo
+plt.subplot(2, 3, 3)
+sns.countplot(x='Survived', hue='Sex', data=titanic_data, palette="Set2")
+plt.title('Contagem de Sobreviventes por Sexo')
+
+# Contagem de embarques por local
+plt.subplot(2, 3, 4)
+sns.countplot(x='Embarked', data=titanic_data, palette="Set2")
+plt.title('Contagem de Embarques por Local')
+
+# Distribuição de tarifas
+plt.subplot(2, 3, 5)
+sns.histplot(titanic_data['Fare'], kde=True, bins=30, color='green')
+plt.title('Distribuição de Tarifas')
+
+plt.tight_layout()
+plt.show()
